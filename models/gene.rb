8e72e435ce0d3a36cd8c1c6d44b8b20e40cca559
Benjamin Mmari <benmmari92@gmail.com>
2020-04-12 13:56:40 +0200

    add gene spec

--- models/gene.rb
+++ models/gene.rb
@@ -4,22 +4,18 @@ class Gene
 
   POSSIBILITIES = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z _)
 
-  def initialize(value:nil)
+  def initialize(value: nil)
     @size = POSSIBILITIES.length
     if value.nil?
       @value = POSSIBILITIES[rand(@size)].upcase
     else
       @value = value
     end
-    raise 'value cannot be null' if @value.nil?
+    raise 'value not in the gene pool' unless POSSIBILITIES.include? @value.downcase
   end
 
   def mutate
     @value = POSSIBILITIES[rand(@size)].upcase
     self
-  end
-
-  def display
-    print @value
   end
 end
